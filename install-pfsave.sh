37b86be9e048ebfb25dc9264de82a923b1a5cd46
Cel A. Skeggs <cela@mit.edu>
2019-11-20 12:58:50 -0500

    Add install script for pfsave

---
+++ install-pfsave.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e -u -o pipefail
+cd "$(dirname "$0")"
+SYSTEMD_SVC="$HOME/.config/systemd/user"
+
+cat >"${SYSTEMD_SVC}/pfsave.service" <<EOF
+[Unit]
+Description=Backup Executor
+
+[Service]
+ExecStart=$PWD/pfsave.py
+
+[Install]
+WantedBy=default.target
+EOF
+
+cat >"${SYSTEMD_SVC}/pfsave.timer" <<EOF
+[Unit]
+Description=Backup Executor Timer
+
+[Timer]
+Persistent=false
+OnBootSec=20min
+OnUnitActiveSec=1h
+Unit=pfsave.service
+
+[Install]
+WantedBy=timers.target
+EOF
+
+systemctl daemon-reload --user
+systemctl enable --user pfsave.timer
+systemctl start --user pfsave.timer
+
