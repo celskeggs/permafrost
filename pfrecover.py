da4a47ab15db68614e8f9b10849fc662eb954c34
Cel A. Skeggs <cela@mit.edu>
2021-05-01 15:26:22 -0400

    Overhaul recovery script for automated use

--- pfrecover.py
+++ pfrecover.py
@@ -5,44 +5,76 @@ import sys
 
 import util
 
+def msg(*text):
+	print(*text, file=sys.stderr)
+	sys.stderr.flush()
+
 def duplicity_restore(source, dest, passphrase):
-	print("performing duplicity restore")
+	msg("performing duplicity restore")
 	nenv = dict(os.environ)
 	nenv["PASSPHRASE"] = passphrase
-	subprocess.check_call(["duplicity", "restore", "--progress", "--", "file://" + os.path.abspath(source), os.path.abspath(dest)], env=nenv)
+	subprocess.check_call(["duplicity", "restore", "--progress", "--", "file://" + os.path.abspath(source), os.path.abspath(dest)], env=nenv, stdout=sys.stderr, stderr=sys.stderr)
 
 PASSFILE="/home/user/.permafrostkey"
 TARGET="/home/user/Materials"
 SOURCE_VM="permathaw"
 READ_SOURCE="/home/user/QubesIncoming/" + SOURCE_VM
+TAR_SOURCE="/home/user/TarUnpack/"
+TAR_TEMP="/home/user/unpack-tarball.tar.bz2"
 
+input_tar = False
 input_passphrase = False
 do_checksum = False
 
+def pull_tarball():
+	total = 0
+	with open(TAR_TEMP, "wb") as f:
+		while True:
+			data = sys.stdin.buffer.read(64 * 1024)
+			if not data: break
+			total += len(data)
+			f.write(data)
+	return total
+
 def backup():
+	source = READ_SOURCE
 	if input_passphrase:
-		passphrase = input("Passphrase> ")
+		msg("Passphrase?")
+		passphrase = sys.stdin.buffer.readline().strip().decode()
+		if not passphrase.isalnum():
+			msg("Not a valid passphrase")
+			sys.exit(1)
+		if input_tar:
+			msg("Reading tarball from stdin...")
+			count = pull_tarball()
+			msg("Unpacking tarball (%d bytes)..." % count)
+			subprocess.check_call(["tar", "-C", TAR_SOURCE, "-x", "--bzip2", "-f", TAR_TEMP], stdout=sys.stderr, stderr=sys.stderr)
+			source = TAR_SOURCE
+			msg("Unpacked!")
 	else:
+		assert not input_tar, "not supported yet: --tar-stdin without --input"
 		with open(PASSFILE, "r") as f:
 			passphrase = f.read().strip()
-	print("beginning...")
+	msg("beginning...")
 	try:
-		duplicity_restore(READ_SOURCE, TARGET, passphrase)
+		duplicity_restore(source, TARGET, passphrase)
 	except Exception as e:
 		raise e
-	print("finished restore!")
+	msg("finished restore!")
 	if do_checksum:
-		print("===== checksums =====")
+		msg("===== checksums =====")
 		subprocess.check_call(["find", "-type", "f", "-exec", "sha256sum", "{}", ";"], cwd=TARGET)
-		print("===== checksums =====")
+		msg("===== checksums =====")
 
 if __name__ == "__main__":
 	for flag in sys.argv[1:]:
 		if flag == "--input":
 			input_passphrase = True
+		elif flag == "--tar-stdin":
+			input_tar = True
 		elif flag == "--checksum":
 			do_checksum = True
 		else:
-			print("Unexpected flag: %r" % flag)
+			msg("Unexpected flag: %r" % flag)
 			sys.exit(1)
 	backup()
